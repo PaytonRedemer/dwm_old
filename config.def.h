40050371b237892b359bf9152699c9ca9bac08da
PaytonRedemer <paytonredemer@gmail.com>
2022-01-22 19:53:22 -0600

    clean v5

--- config.def.h
+++ config.def.h
@@ -1,24 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx  			= 1;        /* border pixel of windows */
-static unsigned int snap      			= 32;       /* snap pixel */
-static const unsigned int gappih        = 12;       /* horiz inner gap between windows */
-static const unsigned int gappiv        = 12;       /* vert inner gap between windows */
-static const unsigned int gappoh        = 12;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov        = 12;       /* vert outer gap between windows and screen edge */
-static       int smartgaps              = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            			= 1;        /* 0 means no bar */
-static int topbar             			= 1;        /* 0 means bottom bar */
-static char font[]            			= "monospace:size=12";
-static char dmenufont[]       			= "monospace:size=10";
-static const char *fonts[]          	= { font };
-static char normbgcolor[]           	= "#222222";
-static char normbordercolor[]       	= "#444444";
-static char normfgcolor[]           	= "#bbbbbb";
-static char selfgcolor[]            	= "#eeeeee";
-static char selbordercolor[]        	= "#005577";
-static char selbgcolor[]            	= "#005577";
+static unsigned int borderpx			= 1;        /* border pixel of windows */
+static unsigned int snap				= 32;       /* snap pixel */
+static const unsigned int gappih		= 12;       /* horiz inner gap between windows */
+static const unsigned int gappiv		= 12;       /* vert inner gap between windows */
+static const unsigned int gappoh		= 12;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov		= 12;       /* vert outer gap between windows and screen edge */
+static       int smartgaps				= 0;        /* 1 means no outer gap when there is only one window */
+static int showbar						= 1;        /* 0 means no bar */
+static int topbar						= 1;        /* 0 means bottom bar */
+static char font[]						= "monospace:size=12";
+static char dmenufont[]					= "monospace:size=10";
+static const char *fonts[]				= { font };
+static char normbgcolor[]				= "#222222";
+static char normbordercolor[]			= "#444444";
+static char normfgcolor[]				= "#bbbbbb";
+static char selfgcolor[]				= "#eeeeee";
+static char selbordercolor[]			= "#005577";
+static char selbgcolor[]				= "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
